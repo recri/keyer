b6af6e80964397baa5ae0952a6ca8e76616ae101
Roger E Critchlow Jr <rec@elf.org>
2012-04-18 23:50:59 -0600

    Renaming, reorganizing, and fixing the dial counts/revolution.

--- lib/sdrui/dial.tcl
+++ lib/sdrui/dial.tcl
@@ -29,13 +29,13 @@ snit::widgetadaptor sdrui::dial {
     # the maximum radius is 1/2 the minimum of width and height
     option -bg {};			# background color of the window containing the dial
 
-    option -radius 90;			# radius of the dial in percent of max
+    option -radius 80;			# radius of the dial in percent of max
     option -fill \#888;			# color of the dial
     option -outline black;		# color of the dial outline
     option -width 3;			# thickness of the dial outline
 
     option -thumb-radius 20;		# radius of the thumb in percent of max
-    option -thumb-position 65;		# center of thumb in percent of max
+    option -thumb-position 50;		# center of thumb in percent of max
     option -thumb-fill \#999;		# color of the thumb
     option -thumb-outline black;	# color of the thumb outline
     option -thumb-width 2;		# thickness of the thumb outline
@@ -44,15 +44,14 @@ snit::widgetadaptor sdrui::dial {
     option -graticule 20;		# number of graticule lines to draw
     option -graticule-radius 95;	# radius of graticule lines in percent of max
     option -graticule-width 3;		# width of graticule lines in pixels
-    option -graticule-fill white;	# 
+    option -graticule-fill black;	# 
 
     option -cpr 1000;			# counts per revolution
 
     option -command {};			# script called to report rotation 
     
     variable data -array {
-	turn 0
-	last-turn 0
+	partial-turn 0
     }
 
     constructor {args} {
@@ -61,8 +60,6 @@ snit::widgetadaptor sdrui::dial {
 	set data(pi) [expr atan2(0,-1)]
 	set data(2pi) [expr {2*$data(pi)}]
 	set data(phi) [expr {-$data(pi)/2}]
-	set data(up-step) [expr {$data(2pi)/$options(-cpr)}]
-	set data(down-step) [expr {-$data(2pi)/$options(-cpr)}]
 	if {$options(-bg) eq {}} {
 	    set options(-bg) [$hull cget -background]
 	}
@@ -75,34 +72,34 @@ snit::widgetadaptor sdrui::dial {
     method {tune} {dir} { $self rotate $data($dir-step) }
 
     method thumb-press {x y} { set data(phi) [expr {atan2($y-$data(yc),$x-$data(xc))}] }
-    method thumb-motion {x y} { $self rotate [expr {atan2($y-$data(yc),$x-$data(xc))-$data(phi)}] }
+    method thumb-motion {x y} {
+	set phi0 $data(phi)
+	set data(phi) [expr {atan2($y-$data(yc),$x-$data(xc))}]
+	$self rotate [expr {$data(phi)-$phi0}]
+    }
 
     method rotate {dphi} {
 	## rotate thumb
 	$self rotate-thumb $dphi
-	## remember last phi
-	set data(phi) [expr {$data(phi)+$dphi}]
-	## compute turn as fraction of rotation
-	set dturn [expr {fmod($dphi,$data(2pi)) / $data(2pi)}]
-	if {$dturn > 0.5} {
-	    set dturn [expr {$dturn-1}]
-	} elseif {$dturn < -0.5} {
-	    set dturn [expr {$dturn+1}]
+
+	## compute rotation in counts 
+	set dturn [expr {$options(-cpr) * $dphi / $data(2pi) + $data(partial-turn)}]
+
+	## account for discontinuity at +/-pi
+	if {$dturn > $options(-cpr)/2} {
+	    set dturn [expr {$dturn-$options(-cpr)}]
+	} elseif {$dturn < -$options(-cpr)/2} {
+	    set dturn [expr {$dturn+$options(-cpr)}]
 	}
-	## compute counts to send to client
-	set data(turn) [expr {$data(turn)+$options(-cpr)*$dturn}]
-	set d [expr {$data(turn)-$data(last-turn)}]
-	if {abs($d) >= 1} {
-	    if {$d > 0} {
-		set d [expr {int($d)}]
-	    } else {
-		set d [expr {-int(-$d)}]
-	    }
-	    set data(turn) [expr {$data(turn)-$d}]
-	    set data(last-turn) $data(turn)
-	    if {$options(-command) ne {}} {
-		{*}$options(-command) $d
-	    }
+
+	## remember leftover partial turn
+	set data(partial-turn) [expr {fmod($dturn,1)}]
+	
+	## see if there is anything to send
+	set dturn [expr {$dturn-$data(partial-turn)}]
+
+	if {abs($dturn) >= 1 && $options(-command) ne {}} {
+	    {*}$options(-command) $dturn
 	}
     }
 
@@ -110,44 +107,56 @@ snit::widgetadaptor sdrui::dial {
 	## get current thumb coordinates
 	foreach {x1 y1 x2 y2} [$hull coords thumb] break
 	## compute the thumb center
-	set x [expr {($x1+$x2)/2-$data(xc)}]
-	set y [expr {($y1+$y2)/2-$data(yc)}]
+	set x [expr {($x1+$x2)/2.0-$data(xc)}]
+	set y [expr {($y1+$y2)/2.0-$data(yc)}]
 	## rotate thumb coordinates
 	set sindphi [expr {sin($dphi)}]
 	set cosdphi [expr {cos($dphi)}]
 	foreach {x y} [list [expr {$x*$cosdphi - $y*$sindphi}] [expr {$y*$cosdphi + $x*$sindphi}]] break
 	## set current thumb coordinates
 	$hull coords thumb [expr {$x-$data(tr)+$data(xc)}] [expr {$y-$data(tr)+$data(yc)}] [expr {$x+$data(tr)+$data(xc)}] [expr {$y+$data(tr)+$data(yc)}]
+	$hull coords pointer [expr {$x*$data(tis)+$data(xc)}] [expr {$y*$data(tis)+$data(yc)}] [expr {$x*$data(tos)+$data(xc)}] [expr {$y*$data(tos)+$data(yc)}]
     }
 
     method window-configure {w h} {
 	#puts "ui-dial window-configure $w $h"
-	set r  [expr {min($w,$h)/2.0}]; # radius of space available
-	set xc [expr {$w/2.0}];	       # center of space available
-	set yc [expr {$h/2.0}];	       # center of space available
-	set dr [expr {$options(-radius)*$r/100.0}];			# dial radius
-	set tr [expr {$r*$options(-thumb-radius)/100.0}];		# thumb radius
-	set tp [expr {$r*$options(-thumb-position)/100.0}];		# thumb position radius
+	set r  [expr {min($w,$h)/2.0}];				# radius of space available
+	set xc [expr {$w/2.0}];					# center of space available
+	set yc [expr {$h/2.0}];					# center of space available
+	set dr [expr {$options(-radius)*$r/100.0}];		# dial radius
+	set tr [expr {$r*$options(-thumb-radius)/100.0}];	# thumb radius
+	set tp [expr {$r*$options(-thumb-position)/100.0}];	# thumb position radius scale
+	set tis [expr {($tp-$tr)/$tp}];				# thumb inner radius scale
+	set tos [expr {($tp+$tr)/$tp}];				# 
 	set gr [expr {$r*$options(-graticule-radius)/100.0}];	# graticule radius
 	set dial [list [expr {$xc-$dr}] [expr {$yc-$dr}] [expr {$xc+$dr}] [expr {$yc+$dr}]]
-	set xt [expr {$xc+$tp*cos($data(phi))}]
-	set yt [expr {$yc+$tp*sin($data(phi))}]
-	set thumb [list [expr {$xt-$tr}] [expr {$yt-$tr}] [expr {$xt+$tr}] [expr {$yt+$tr}]]
-	set graticule {}
+	set x [expr {$tp*cos($data(phi))}]
+	set y [expr {$tp*sin($data(phi))}]
+	set thumb [list [expr {$x-$tr+$xc}] [expr {$y-$tr+$yc}] [expr {$x+$tr+$xc}] [expr {$y+$tr+$yc}]]
+	set pointer [list [expr {$x*$tis+$xc}] [expr {$y*$tis+$yc}] [expr {$x*$tos+$xc}] [expr {$y*$tos+$yc}]]
+
 	if {$options(-graticule) <= 0} {
-	    lappend graticule $xc $yc $xc $yc
+	    set graticule [list $xc $yc $xc $yc]
+	    set mask [list $xc $yc $xc $yc]
 	} else {
+	    set graticule {}
 	    set p 0
 	    set dp [expr {$data(2pi)/$options(-graticule)}]
 	    for {set i 0} {$i < $options(-graticule)} {incr i} {
-		lappend graticule $xc $yc [expr {$xc+$gr*cos($p)}] [expr {$yc+$gr*sin($p)}]
+		set x [expr {$xc+$gr*cos($p)}]
+		set y [expr {$yc+$gr*sin($p)}]
+		lappend graticule $xc $yc $x $y
 		set p [expr {$p+$dp}]
 	    }
+	    set ir [expr {($dr+$gr)/2.0}]
+	    set mask [list [expr {$xc-$ir}] [expr {$yc-$ir}] [expr {$xc+$ir}] [expr {$yc+$ir}]]
 	}
 	if {[llength [$hull find withtag thumb]] == 0} {
 	    $hull create line $graticule -tag graticule -fill $options(-graticule-fill) -width $options(-graticule-width)
+	    $hull create oval $mask -tag mask -fill $options(-bg) -outline {} 
 	    $hull create oval $dial -tag dial -fill $options(-fill) -outline $options(-outline) -width $options(-width) 
 	    $hull create oval $thumb -tag thumb -fill $options(-thumb-fill) -outline $options(-thumb-outline) -activewidth $options(-thumb-activewidth) -width $options(-thumb-width)
+	    $hull create line $pointer -tag pointer -fill $options(-thumb-outline) -width $options(-thumb-width)
 	    $hull bind thumb <ButtonPress-1> [mymethod thumb-press %x %y]
 	    $hull bind thumb <B1-Motion> [mymethod thumb-motion %x %y]
 	} else {
@@ -155,6 +164,6 @@ snit::widgetadaptor sdrui::dial {
 	    $hull coords dial $dial
 	    $hull coords thumb $thumb
 	}
-	array set data [list yc $yc xc $xc tr $tr]
+	array set data [list yc $yc xc $xc tr $tr tis $tis tos $tos]
     }
 }    
